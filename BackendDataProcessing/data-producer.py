279baeb9849cf2edf0935fe38723d9b35cc5d065
YaokaiYang-assaultmaster <yxy586@case.edu>
2017-07-23 19:41:51 -0700

    Improve the doc and argparse code for data-producer.py, modified README correspondingly

--- BackendDataProcessing/data-producer.py
+++ BackendDataProcessing/data-producer.py
@@ -23,17 +23,15 @@ import time
 
 logging.basicConfig()
 logger = logging.getLogger('data-producer')
-logger.setLevel(logging.DEBUG)
 
-symbol = 'AAPL'
-topic_name = 'stock'
-kafka_broker = '127.0.0.1:9092'
+DEFAULT_SYMBOL = 'AAPL'
+DEFAULT_TOPIC_NAME = 'stock'
+DEFAULT_KAFKA_BROKER = '127.0.0.1:9092'
+DEFAULT_LOG_LEVEL = 'DEBUG'
 
 
 def shutdown_hook():
-    """Summary
-
-    Release resorces at exiting
+    """Release resorces at exiting
     Speecifically, releases kafka producer and scheduler
     """
     try:
@@ -47,9 +45,7 @@ def shutdown_hook():
 
 
 def fetch_price(producer, symbol):
-    """Summary
-
-    Use googlefinance's getQuotes() api to quote a stock's info.
+    """Use googlefinance's getQuotes() api to quote a stock's info.
     Store the info into Kafka
 
     Args:
@@ -60,43 +56,72 @@ def fetch_price(producer, symbol):
     try:
         stock_info = json.dumps(getQuotes(symbol))
 
-        logger.debug('received stock price %s' % (stock_info))
+        logger.debug('received stock price for {}'.format(symbol))
         producer.send(topic=topic_name, value=stock_info,
                       timestamp_ms=time.time())
+        logger.debug('sending stock price for {}'.format(symbol))
     except KafkaTimeoutError as timeout_error:
         logger.warn(
-            'failed to send stock price for %s to kafka due to timeout'
-            % (symbol))
+            'failed to send stock price for {} to kafka due to timeout'
+            .format(symbol))
         print(timeout_error)
     except Exception:
-        logger.warn(
-            'failed to send stock price for %s due to unknown reason'
-            % (symbol))
+        logger.error(
+            'failed to send stock price for {} due to unknown reason'
+            .format(symbol))
 
 
 if __name__ == '__main__':
     # - argument parser
     parser = argparse.ArgumentParser()
-    parser.add_argument('symbol', help='the symbol of the stock')
-    parser.add_argument('topic_name', help='the kafka topic to push to')
-    parser.add_argument('kafka_broker', help='the location of kafka broker')
+    parser.add_argument(
+        '--symbol',
+        help='the symbol of the stock',
+        default=DEFAULT_SYMBOL,
+    )
+    parser.add_argument(
+        '--topic_name',
+        help='the kafka topic to push to',
+        default=DEFAULT_TOPIC_NAME,
+    )
+    parser.add_argument(
+        '--kafka_broker',
+        help='the location of kafka broker',
+        default=DEFAULT_KAFKA_BROKER,
+    )
+    parser.add_argument(
+        '--log_level',
+        help='level of the log file',
+        default=DEFAULT_LOG_LEVEL,
+    )
 
     args = parser.parse_args()
     symbol = args.symbol
     topic_name = args.topic_name
     kafka_broker = args.kafka_broker
+    str_level = args.log_level
+    logger_level = logging.getLevelName(str_level)
+    logger.setLevel(logger_level)
 
     producer = KafkaProducer(bootstrap_servers=kafka_broker)
 
     schedule = BackgroundScheduler()
     schedule.add_executor('threadpool')
 
-    schedule.add_job(fetch_price, 'interval', [
-                     producer, symbol], seconds=1, id=symbol)
+    schedule.add_job(
+        fetch_price,
+        'interval',
+        [producer, symbol],
+        seconds=1,
+        id=symbol,
+    )
 
     schedule.start()
 
     atexit.register(shutdown_hook)
 
+    # Use while true loop to prevent the program from
+    # exiting, allowing the scheduler to exetuing
+    # fetch_price task.
     while True:
         pass
